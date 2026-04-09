9bebe1dc0509bc316d84bd1c2866b5db7b3271c4
renanfmorato <renan.fmorato@gmail.com>
2026-04-09 16:03:09 -0300

    Hackathon

---
+++ HACKATON/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("Eae");
+}
+for (int j = 0; j < 10; j++)
+{
+    Console.WriteLine("Oi Brenno");
+}
+
