c711c020e29fe1b33607e092c63bdb2c5b4e362c
renanfmorato <renan.fmorato@gmail.com>
2026-04-09 16:20:15 -0300

    PA

---
+++ PA/Program.cs
@@ -0,0 +1,8 @@
+﻿int a1 = 10; // Ataque inicial
+int r = 5;   // Ganho por nível
+int n = 20;  // Nível desejado
+
+int an = a1 + (n - 1) * r;
+
+Console.WriteLine("O ataque no nível " + n + " será: " + an);
+
