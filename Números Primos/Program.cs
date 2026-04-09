201f2839a4347b6e7da82ce2b8e526c99a832e1b
renanfmorato <renan.fmorato@gmail.com>
2026-04-09 16:16:41 -0300

    Números Primos

---
+++ Números Primos/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int contador = 0, resultado;
+Console.WriteLine("Calculadora de Números Primos");
+Console.WriteLine("Digite o número");
+int primo = 7;
+for (int i = 1; i <= primo; i++)
+{
+
+    Console.WriteLine($"{i}");
+}
+Console.Write("Fim");
