24bcab118446e5d042bd1e7d719c773da870db9a
renanfmorato <renan.fmorato@gmail.com>
2026-04-09 15:50:20 -0300

    Sequência de Fibonacci

---
+++ Imc/Sequência de Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Usando as linguagens (tipos e comandos) solicitadas
+        int quantidade;
+        int n1 = 0;
+        int n2 = 1;
+        int proximo;
+
+        Console.Write("Digite quantos números da sequência você deseja ver: ");
+
+        // Console.ReadLine para capturar a entrada do usuário
+        string entrada = Console.ReadLine();
+        quantidade = int.Parse(entrada);
+
+        Console.WriteLine("Sequência de Fibonacci:");
+
+        // O 'for' é necessário para automatizar a repetição
+        for (int i = 0; i < quantidade; i++)
+        {
+            Console.WriteLine(n1);
+
+            // Lógica da soma
+            proximo = n1 + n2;
+            n1 = n2;
+            n2 = proximo;
+        }
+    }
+}
